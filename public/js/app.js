09e0833f55bbd2bf5c9c11f83f13607c3fdd61bf
e-Coucou <eric@e-coucou.com>
2016-11-09 15:39:45 +0100

    maj d3

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ angular.module("rky10App", ['ngRoute'])
       })
       .when("/graph", {
         templateUrl: "graph.html",
-        controller: "GrapController",
+        controller: "GraphController",
         resolve: {
           data : function(Data) {
               return "coucou";
@@ -32,7 +32,7 @@ angular.module("rky10App", ['ngRoute'])
   })
   .controller("ListController", function(sensors, $scope) {
     $scope.sensors = sensors.data;
-  });
+  })
   
   .controller("GraphController", function(data, $scope) {
     $scope.data = "RAS";
