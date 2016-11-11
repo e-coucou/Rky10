7c6df0fc63ca26d7984595929771f0a375ec5abd
e-coucou <eric@ecoucou.com>
2016-11-11 20:29:31 +0100

    maj

--- public/js/testRky.js
+++ public/js/testRky.js
@@ -6,10 +6,10 @@
       $scope.title = "testRky";
       $scope.d3Data = [
         {capteur: "Rky_L", value:12},
-        {capteur: "Rky_L", value:13},
-        {capteur: "Rky_L", value:19},
-        {capteur: "Rky_L", value:34},
-        {capteur: "Rky_L", value:55}
+        {capteur: "Rky_T", value:13},
+        {capteur: "Rky_P", value:19},
+        {capteur: "Rky_Lu", value:34},
+        {capteur: "Rky_Ya", value:55}
       ];
     }]);
 
