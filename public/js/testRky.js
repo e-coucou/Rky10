04f906735c6a3cefb9b9535c7d669886912da618
e-coucou <eric@ecoucou.com>
2016-11-11 19:39:50 +0100

    d3 et Data

---
+++ public/js/testRky.js
@@ -0,0 +1,16 @@
+(function () {
+  'use strict';
+
+  angular.module('RkyApp.controllers')
+    .controller('testRky', ['$scope', function($scope){
+      $scope.title = "testRky";
+      $scope.d3Data = [
+        {sensors: "Rky_L", value:12},
+        {sensors: "Rky_L", value:13},
+        {sensors: "Rky_L", value:19},
+        {sensors: "Rky_L", value:34},
+        {sensors: "Rky_L", value:55}
+      ];
+    }]);
+
+}());
