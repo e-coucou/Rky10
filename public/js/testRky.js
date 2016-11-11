96b585982e6f9d29699a32c5dc3487f377b126ed
e-coucou <eric@ecoucou.com>
2016-11-11 20:36:21 +0100

    maj

--- public/js/testRky.js
+++ public/js/testRky.js
@@ -7,6 +7,7 @@
       $scope.d3Data = [
         {title: "Rky_L", score:12},
         {title: "Rky_L", score:43},
+        {title: "Rky_L", score:10},
         {title: "Rky_L", score: 87}
       ];
     }]);
