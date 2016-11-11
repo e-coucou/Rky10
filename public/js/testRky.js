dc8f9df02f7fac9eef4c5d46f6627ca6452e145a
e-coucou <eric@ecoucou.com>
2016-11-11 23:16:41 +0100

    live data

--- public/js/testRky.js
+++ public/js/testRky.js
@@ -5,11 +5,25 @@
     .controller('testRky', ['$scope', function($scope){
       $scope.title = "testRky";
       $scope.d3Data = [
-        {capteur: "Rky_L", score:12},
-        {capteur: "Rky_L", score:43},
         {capteur: "Rky_L", score:10},
         {capteur: "Rky_L", score: 87}
       ];
+    }]);
+
+}());
+
+
+(function () {
+  'use strict';
+
+  angular.module('RkyApp.controllers')
+    .controller('httpRky', ['$scope', function($scope){
+      $scope.title = "httpRky";
+var url = "https://rky10.herokuapp.com/api/v1/list/scale";
+$http.get(url)
+.then(function(response){
+  console.log(response.data);
+});
     }]);
 
 }());
