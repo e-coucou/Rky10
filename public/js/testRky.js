b64ae1da1db3f6d8aec111e3ba99007075a2a62e
e-coucou <eric@ecoucou.com>
2016-11-11 20:45:31 +0100

    maj

--- public/js/testRky.js
+++ public/js/testRky.js
@@ -5,10 +5,10 @@
     .controller('testRky', ['$scope', function($scope){
       $scope.title = "testRky";
       $scope.d3Data = [
-        {title: "Rky_L", score:12},
-        {title: "Rky_L", score:43},
-        {title: "Rky_L", score:10},
-        {title: "Rky_L", score: 87}
+        {name: "Rky_L", score:12},
+        {name: "Rky_L", score:43},
+        {name: "Rky_L", score:10},
+        {name: "Rky_L", score: 87}
       ];
     }]);
 
