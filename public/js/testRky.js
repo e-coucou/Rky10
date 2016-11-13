15c584e7b7a4f5967a15c2d9fcfb09248bd74653
e-coucou <eric@ecoucou.com>
2016-11-13 22:58:56 +0100

    date

--- public/js/testRky.js
+++ public/js/testRky.js
@@ -21,8 +21,8 @@
     .controller('httpRky', ['$scope', '$http', function($scope, $http){
       $scope.title = "httpRky ... in progress";
       var url1 = "/api/v1/list/scale";
-      var url ="/api/v1/search?name=Rky_P&source=*&after=2016/11/10" + $scope.date;
-      console.log(url);
+      var url ="/api/v1/search?name=Rky_P&source=*&after=2016/11/10"; // + $scope.date;
+      console.log($scope.date);
       var data = [];
       $http.get(url)
         .then(function(response){
