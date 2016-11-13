e48aa1125d9a1bd199b75b3834e12df13bc7dab5
e-coucou <eric@ecoucou.com>
2016-11-13 13:44:33 +0100

    map again

--- public/js/d3Basic.js
+++ public/js/d3Basic.js
@@ -132,9 +132,9 @@
               // this can also be found dynamically when the data is not static
               //
 //            max = Math.max.apply(Math, _.map(data, ((val)-> val.count)))
-            console.log(data.score);
-            max = Math.max.apply(Math, data.score);
-//            max = Math.max.apply(Math, data.score.map(data.score, function(x) {return x.count; }));
+            console.log(data.map(function(o) { return o.score; });
+//            max = Math.max.apply(Math, data.score);
+            max = Math.max.apply(Math, data.map(function(x) {return x.score; }));
               console.log(max);
 
             // set the height based on the calculations above
