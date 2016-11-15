a60232e631b2f2d7fa8cd7a016a942ee9f27c077
e-coucou <eric@ecoucou.com>
2016-11-15 22:31:33 +0100

    et et

--- public/js/testRky.js
+++ public/js/testRky.js
@@ -23,6 +23,7 @@
       $scope.after = "2016/11/01";
       $scope.before = "2016/11/02";
       $scope.moyenne = 0.0;
+      $scope.somme = 0.0;
       $scope.nb = 0;
       var url1 = "/api/v1/list/scale";
       var urlBase ="/api/v1/search?name=Rky_P&source=*";
@@ -34,21 +35,22 @@
         .then(function(response){
         angular.forEach(response.data,function(value){
           data.push({ capteur: value.heure, score: value.value });
-          $scope.moyenne = $scope.moyenne + parseFloat(value.value); 
-          console.log(parseFloat(value.value),$scope.moyenne);
+          $scope.somme = $scope.somme + parseFloat(value.value); 
+          console.log(parseFloat(value.value),$scope.somme);
           $scope.nb = $scope.nb + 1;
          });
        });
        console.log($scope);
       }
 
+      $scope.moyenne = $scope.somme / $scope.nb;
       $scope.d3Data = data;
 
       $scope.clickButton = function() {
           url = urlBase +"&after="+ $scope.after+"&before=" +$scope.before;
           data.length = 0;
           $scope.nb=0;
-          $scope.moyenne=0.0;
+          $scope.somme=0.0;
           console.log(url);
           console.log($scope.moyenne);
           getData();
