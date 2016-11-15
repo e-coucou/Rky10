ee931727afee90a5b40d1f409bfec4811b6b89c3
e-coucou <eric@ecoucou.com>
2016-11-16 00:06:52 +0100

    et beh

--- public/js/testRky.js
+++ public/js/testRky.js
@@ -7,7 +7,7 @@
       $scope.title = "selectRky";
       $scope.data =  { 
         model: null,
-        availableOptions: = $http.get("/api/v1/name")
+        availableOptions: $http.get("/api/v1/name")
         .then(function(response) {
             return response;
         }, function(response) {
