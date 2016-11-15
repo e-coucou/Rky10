a93c2f7b9343d2cc7ceb2225453707c168d43593
e-coucou <eric@ecoucou.com>
2016-11-15 21:15:59 +0100

    stroke

--- public/js/d3Basic.js
+++ public/js/d3Basic.js
@@ -156,6 +156,7 @@
                 }) // height + margin between bars
                 .transition()
                   .duration(1000) // time of duration
+                  .stroke(rgb(0,0,0))
                   .attr("fill", function(d) {
                     var val = ((d.score -min)/((max-min)/255));
 //                    console.log(val);
