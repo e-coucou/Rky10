eb5ec666bb25da6957eaa0e6a45c6cfc517a8f92
e-coucou <eric@ecoucou.com>
2016-11-16 19:39:48 +0100

    test

--- public/js/testRky.js
+++ public/js/testRky.js
@@ -5,8 +5,8 @@
     .controller('selectRky', ['$scope','$http', function($scope,$http){
       var url = "/api/v1/list/scale";
       $scope.title = "selectRky";
-      var liste =  return function(Select) {
-              return Select.getScale();};
+      var liste =  { function(Select) {
+              return Select.getScale();}};
       console.log(liste);
 
       $scope.data =  { 
