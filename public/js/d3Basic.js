13ad12fc207a065e157ec2fca1d32f7a6f164fb9
e-coucou <eric@ecoucou.com>
2016-11-19 09:27:42 +0100

    color

--- public/js/d3Basic.js
+++ public/js/d3Basic.js
@@ -187,7 +187,7 @@ var curseur = d3.svg.line()
                   .attr("stroke-width",0)
                   .attr("fill", function(d) {
                     var val = ((d.value -min)/((max-min)/16));
-                    return "rgb(0, 0, " + val*10+90 + ")"; 
+                    return "rgb(0, 0, " + val*5+150 + ")"; 
                     })
                   .attr("height", function (d) { return h(d); }) // half of the 20 side margin specified above
                   .attr("y", function (d) { return y(d);}); // width based on scale
