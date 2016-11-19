d0d1283352c1a2c51e1947e16a38a774c5f4816f
e-coucou <eric@ecoucou.com>
2016-11-19 10:19:10 +0100

    date

--- public/js/testRky.js
+++ public/js/testRky.js
@@ -85,7 +85,7 @@
       }
 
       $scope.clickButton = function() {
-        $scope.jour = $filter('date')(new Date(),'DD/MM/YYYY');
+        $scope.jour = $filter('date')(new Date(),"DD/MM/YYYY");
         console.log($scope.jour);
           updateData();
         }
