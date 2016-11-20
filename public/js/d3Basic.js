18c2fec747a4a434f6021f986088b81f460c2868
e-coucou <eric@ecoucou.com>
2016-11-20 21:55:46 +0100

    maj yx

--- public/js/d3Basic.js
+++ public/js/d3Basic.js
@@ -186,18 +186,22 @@ var repere_moy = d3.svg.line()
     .y(function(d) { return d.y; });
 
 // Add a y-axis label.
-var yScale = d3.scale.linear().domain([min, max]).range([0,height]);
-var yAxis = d3.svg.axis().scale(yScale).orient("left");
+var yScale = d3.scale.linear().domain([min, max]).range([0,height]),
+    yAxis = d3.svg.axis().scale(yScale).orient("right");
+
+console.log(yScale);
+
 svg.append("g")
     .attr("class", "y axis")
     .call(yAxis);
+    
 svg.append("text")
     .attr("class", "y label")
     .attr("text-anchor", "end")
     .attr("y", 6)
     .attr("dy", ".75em")
     .attr("transform", "rotate(-90)")
-    .text("classement");
+    .text("mesure");
 
             //create the rectangles for the bar chart
             svg.selectAll("rect")
