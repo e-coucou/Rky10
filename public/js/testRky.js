32a79c55dc02f8b6046a21222e03535176a1f053
e-coucou <eric@ecoucou.com>
2016-11-20 14:14:55 +0100

    yop

--- public/js/testRky.js
+++ public/js/testRky.js
@@ -104,7 +104,7 @@
         console.log('update DATA');
         $scope.after = $filter('date')(partage.dateFrom,"yyyy/MM/dd");
         $scope.before = $filter('date')(partage.dateTo,"yyyy/MM/dd");
-        console.log((partage.dateTo-partage.dateFrom)/24/60/60);
+        console.log("delta de ",(partage.dateTo-partage.dateFrom)/24/60/60/1000," jours");
         urlBase = "/api/v1/search?name="+partage.capteur+"&source=*";
         url = urlBase +"&after="+ $scope.after+"&before=" +$scope.before;
           data.length = 0;
