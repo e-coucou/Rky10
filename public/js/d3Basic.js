06ba5e5d5cccd48f32ea8a43290425f331a35797
e-coucou <eric@ecoucou.com>
2016-11-21 21:28:36 +0100

    update

--- public/js/d3Basic.js
+++ public/js/d3Basic.js
@@ -138,9 +138,7 @@
             function info(d,i) {
               var s_date = d.date.split('/');
               var s_heure = d.heure.split(':');
-              console.log(s_date, s_heure);
               var date_text = s_date[1]+'/'+s_date[0]+'\n'+s_heure[0]+':'+s_heure[1];
-              console.log(date_text);
               label.text(Math.round(d.value*100)/100) ;
               var datac = [ { "x" : x(d,i), "y" : 20 }, { "x" : x(d,i), "y" : height } ];
               d3.selectAll(".curseur").remove();
