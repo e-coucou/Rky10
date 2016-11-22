e5cc9efefbac74a20b6fcacb83e4c46270ddfc5b
e-coucou <eric@ecoucou.com>
2016-11-22 23:04:41 +0100

    width

--- public/js/d3Basic.js
+++ public/js/d3Basic.js
@@ -106,7 +106,8 @@
         // creation du contenair
         var svg = d3.select(iElement[0])
               .append("svg")
-              .attr("width", width + margin.left + margin.right)
+              .attr("width", "100%")
+//              .attr("width", width + margin.left + margin.right)
               .attr("height", height + margin.top + margin.bottom)
             .append("g")
               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
