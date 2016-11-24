1cfa66056ff2aca831f343832ca67acfe2865426
e-coucou <eric@ecoucou.com>
2016-11-24 21:58:10 +0100

    error

--- public/js/homeCtrl.js
+++ public/js/homeCtrl.js
@@ -19,13 +19,39 @@
   'use strict';
 
   angular.module('RkyApp.controllers')
-    .controller('newRky', ['$scope', function($scope){
+    .controller('newRky', ['$scope', '$scope', function($scope, $http){
       $scope.title = "Listes des Capteurs";
       $scope.capteur = { tag:'', info:'', name:'', unit:''};
       $scope.clickButton = function(){
         alert($scope.capteur.name,'/',$scope.capteur.tag);
         console.log($scope.capteur);
       };
+
+            $scope.SendData = function () {
+           // use $.param jQuery function to serialize data from JSON 
+            var data = $.param({
+                fName: $scope.firstName,
+                lName: $scope.lastName
+            });
+        
+            var config = {
+                headers : {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            }
+
+            $http.post('/ServerRequest/PostDataResponse', data, config)
+            .success(function (data, status, headers, config) {
+                $scope.PostDataResponse = data;
+            })
+            .error(function (data, status, header, config) {
+                $scope.ResponseDetails = "Data: " + data +
+                    "<hr />status: " + status +
+                    "<hr />headers: " + header +
+                    "<hr />config: " + config;
+            });
+        };
+
     }]);
 
 }());
