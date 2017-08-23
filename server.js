1d9cd5dec0f1d07c43dedd87aaa6c92338df39ef
e-Coucou <eric@e-coucou.com>
2017-08-23 14:06:20 +0200

    wh reply

--- server.js
+++ server.js
@@ -104,11 +104,9 @@ app.post("/api/v1/scale", function(req, res) {
 app.post("/api/v1/webhook", function(req,res) {
 	console.log(req.body.result.fulfillment.speech);
 	req.body.result.fulfillment.speech = "Je connais la réponse";
-	req.body.status.code = 200;
-	req.body.errtype = "Success";
+//	req.body.result.
 	console.log(req.body);
-	res.status(200);
-	res.json(req.body);
+	res.status(200).json(req.body);
 //	res.render('index', { title: 'WebHook Info' });
 });
 
