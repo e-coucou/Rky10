45f708bb08d4286f1ef108fc8f919ea04ab68f38
e-Coucou <eric@e-coucou.com>
2017-08-23 11:41:17 +0200

    wh

--- server.js
+++ server.js
@@ -103,8 +103,8 @@ app.post("/api/v1/scale", function(req, res) {
 // WebHook ?!
 app.post("/api/v1/webhook", function(req,res) {
 	console.log(req.body);
-//	res.status(200);
-	res.render('index', { title: 'WebHook Info' });
+	res.status(200);
+//	res.render('index', { title: 'WebHook Info' });
 });
 
 /*  "/contacts/:id"
